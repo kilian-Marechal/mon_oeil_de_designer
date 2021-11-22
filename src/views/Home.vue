35d91c915e8dc4433ddc91268c605629df293204
Kilian <marechal.kilian@gmail.com>
2021-11-22 17:46:03 +0100

    Start Button
    Also deleted HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <PrimaryButton href="#" to="to" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PrimaryButton from "@/components/PrimaryButton.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
-  },
+    PrimaryButton
+  }
 };
 </script>
