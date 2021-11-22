591c933d9c60794e2b7527f2da25cefdffd9bcf1
Kilian <marechal.kilian@gmail.com>
2021-11-22 15:58:41 +0100

    Pushed Base Files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    color: $dark;
+
+    &.router-link-exact-active {
+      color: $primary;
+    }
+  }
+}
+</style>
